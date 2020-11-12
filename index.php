2f3436b7a257805b124d4bf5a59aefee18a14334
gustavoGajdeczka <tavinhogustavog@gmail.com>
2020-11-12 01:32:02 -0300

    menu categorias

--- index.php
+++ index.php
@@ -280,6 +280,19 @@ $app->post("/admin/categories/:idcategory", function($idcategory){
 	exit;
 });
 
+$app->get("/categories/:idcategory", function($idcategory){
+	$category = new Category();
+
+	$category->get((int)$idcategory);
+
+	$page = new Page();
+
+	$page->setTpl("category", [
+		'category'=>$category->getValues(),
+		'products'=>[]
+	]);
+});
+
 // ## Execução do SITE
 $app->run();
 
